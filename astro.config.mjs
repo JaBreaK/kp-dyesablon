7fb519bf3514e319eeaf9a2081d0d40e52d59460
JaBreaK <114270384+JaBreaK@users.noreply.github.com>
2025-05-13 10:34:47 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 import alpinejs from '@astrojs/alpinejs';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  // Enable SSR for real-time data fetching
+  output: 'server',
+  adapter: vercel(),
+
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [
-    alpinejs()
-  ]
+    alpinejs(),
+  ],
 });
