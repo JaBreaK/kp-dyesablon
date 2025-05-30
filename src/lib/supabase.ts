9646f4de31c882ddbcdc0f900f6130d6606dd81b
JaBreaK <114270384+JaBreaK@users.noreply.github.com>
2025-05-30 15:32:31 +0700

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,6 @@
-// lib/supabase.ts
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-// Ambil dari env
-const supabaseUrl  = import.meta.env.PUBLIC_SUPABASE_URL as string;
-const supabaseKey  = import.meta.env.PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = 'https://pmfzypvynmyotmvbgafi.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZnp5cHZ5bm15b3RtdmJnYWZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxNTcxNTgsImV4cCI6MjA2MTczMzE1OH0.NimEhbTS9Rz_GGXpEABU_rWunPB6TUN7S4ufTS7FNiM' // ambil dari Supabase -> Project -> API -> anon key
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseKey)
